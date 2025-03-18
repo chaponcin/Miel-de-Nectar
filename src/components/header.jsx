492a3ec5331cfb4b305a435ebd7c77ac26eb35fc
Charlotte P <charlotteponcin@hotmail.com>
2025-03-18 16:57:09 +0100

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import './header.css'; 
-import logo from '../assets/logo.png'; // Make sure to import the logo
+import logo from '../assets/logo.png'; 
 
 function Header() {
   return (
     <header className="honey-header">
       <div className="logo">
-        <img className="item-z" src={logo} alt="Rucher de Stoqueu Logo" />
+        <img className="red" src={logo} alt="Rucher de Stoqueu Logo" />
       </div>
       <nav>
-        <ul className="nav-list">
-          <li className="item-a"><a href="/panier">Panier</a></li>
-          <li className="item-b"><a href="/register">Register</a></li>
-          <li className="item-c"><a href="/login">Login</a></li>
-          <li className="item-e"><a href="/boutique">Boutique</a></li>
-          <li className="item-d"><a href="/info">Qui sommes-nous</a></li>
-          <li className="item-f"><a href="/contact">Contact</a></li>
+        <ul className="nav-list mondrian-container">
+          <li className="lightgrayone"><a href="/panier">Panier</a></li>
+          <li className="lightgraytwo"><a href="/register">Register</a></li>
+          <li className="lightgraythree"><a href="/login">Login</a></li>
+          <li className="blue"><a href="/boutique">Boutique</a></li>
+          <li className="lightgrayfour"><a href="/info">Qui sommes-nous</a></li>
+          <li className="yellow"><a href="/contact">Contact</a></li>
         </ul>
       </nav>
-      <div class="mondrian-container">
-              <div class="block red"></div>
-              <div class="block lightgrayone">Panier</div>
-              <div class="block lightgraytwo">Register</div>
-              <div class="block lightgraythree">Login</div>
-              <div class="block blue">Boutique</div>
-              <div class="block lightgrayfour">Qui sommes-nous</div>
-              <div class="block yellow">Contact</div>
- 
-          </div>
+
     </header>
   );
 }
